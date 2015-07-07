2b2dbb0d105dc3811967728f8437c96308bf98d8
giliy <gili.yohanan@gmail.com>
2015-07-07 23:25:06 +0300

    last update 7.7

--- app/src/main/java/com/barcodescanner/gili/scan9/MainActivity.java
+++ app/src/main/java/com/barcodescanner/gili/scan9/MainActivity.java
@@ -4,6 +4,10 @@ package com.barcodescanner.gili.scan9;
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.PixelFormat;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -34,9 +38,6 @@ import java.util.List;
 
 public class MainActivity extends Activity implements LoaderCallbacks<Cursor> {
 
-    private static final String DUMMY_CREDENTIALS = "user@test.com:12345";
-
-    private UserLoginTask userLoginTask = null;
     private View progressView;
     private AutoCompleteTextView usernameTextView;
     private EditText passwordTextView;
@@ -48,6 +49,15 @@ public class MainActivity extends Activity implements LoaderCallbacks<Cursor> {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getWindow().getDecorView().getBackground().setDither(true);
+        getWindow().setFormat(PixelFormat.RGBA_8888);
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+        Bitmap gradient = BitmapFactory.decodeResource(getResources(), R.drawable.supermarket, options);
+
+        findViewById(R.id.loginView).setBackgroundDrawable(new BitmapDrawable(gradient));
+
         usernameTextView = (AutoCompleteTextView)findViewById(R.id.user);
         loadAutoComplete();
 
@@ -56,225 +66,30 @@ public class MainActivity extends Activity implements LoaderCallbacks<Cursor> {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
                 if (id == EditorInfo.IME_NULL) {
-                    initLogin();
                     return true;
                 }
                 return false;
             }
         });
-
-        loginButton = (Button) findViewById(R.id.btnLogin);
-        loginButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                initLogin();
-            }
-        });
-
-        progressView = findViewById(R.id.login_progress);
-
-        signUpButton = (Button) findViewById(R.id.btnReg);
-        signUpButton.setPaintFlags(signUpButton.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
-        Linkify.addLinks(signUpButton, Linkify.ALL);
-
-        signUpButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.i("LoginActivity", "Sign Up Activity activated.");
-                // this is where you should start the signup Activity
-                 MainActivity.this.startActivity(new Intent(MainActivity.this, RegisterScreen.class));
-            }
-        });
-    }
-
-    public void initLogin() {
-        if (userLoginTask != null) {
-            return;
-        }
-
-        usernameTextView.setError(null);
-        passwordTextView.setError(null);
-
-        String username = usernameTextView.getText().toString();
-        String password = passwordTextView.getText().toString();
-        boolean cancelLogin = false;
-        View focusView = null;
-
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
-            passwordTextView.setError("Invalid Password");
-            focusView = passwordTextView;
-            cancelLogin = true;
-        }
-
-        if (TextUtils.isEmpty(username)) {
-            usernameTextView.setError("Empty Username");
-            focusView = usernameTextView;
-            cancelLogin = true;
-        } else if (!isEmailValid(username)) {
-            usernameTextView.setError("Invalid Username");
-            focusView = usernameTextView;
-            cancelLogin = true;
-        }
-
-        if (cancelLogin) {
-            // error in login
-            focusView.requestFocus();
-        } else {
-            // show progress spinner, and start background task to login
-            showProgress(true);
-            userLoginTask = new UserLoginTask(username, password);
-            userLoginTask.execute((Void) null);
-        }
-    }
-
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-    public void showProgress(final boolean show) {
-        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
-        // for very easy animations. If available, use these APIs to fade-in
-        // the progress spinner.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
-
-            progressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            progressView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    progressView.setVisibility(show ? View.VISIBLE : View.GONE);
-                }
-            });
-        } else{
-            Log.i("DEBUG:", "The api doesnt suppot this animation");
-            // The ViewPropertyAnimator APIs are not available, so simply show
-            // and hide the relevant UI components.
-            progressView.setVisibility(show ? View.VISIBLE : View.GONE);
-
-        }
-    }
-
-    private boolean isPasswordValid(String password) {
-        //add your own logic
-        return password.length() > 4;
-    }
-
-    private boolean isEmailValid(String email) {
-        //add your own logic
-        return email.contains("@");
     }
 
     private void loadAutoComplete() {
         getLoaderManager().initLoader(0, null, this);
     }
 
+
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        return new CursorLoader(this,
-                // Retrieve data rows for the device user's 'profile' contact.
-                Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
-                        ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
-
-                // Select only email addresses.
-                ContactsContract.Contacts.Data.MIMETYPE +
-                        " = ?", new String[]{ContactsContract.CommonDataKinds.Email
-                .CONTENT_ITEM_TYPE},
-
-                // Show primary email addresses first. Note that there won't be
-                // a primary email address if the user hasn't specified one.
-                ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
-    }
-
-    private interface ProfileQuery {
-        String[] PROJECTION = {
-                ContactsContract.CommonDataKinds.Email.ADDRESS,
-                ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
-        };
-
-        int ADDRESS = 0;
-        int IS_PRIMARY = 1;
+        return null;
     }
 
     @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
-        List<String> emails = new ArrayList<>();
-        cursor.moveToFirst();
-        while (!cursor.isAfterLast()) {
-            emails.add(cursor.getString(ProfileQuery.ADDRESS));
-            cursor.moveToNext();
-        }
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
 
-        addEmailsToAutoComplete(emails);
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
 
     }
-
-    private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
-        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
-        ArrayAdapter<String> adapter =
-                new ArrayAdapter<>(MainActivity.this,
-                        android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
-
-        usernameTextView.setAdapter(adapter);
-    }
-
-    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
-
-        private final String emailStr;
-        private final String passwordStr;
-
-        UserLoginTask(String email, String password) {
-            emailStr = email;
-            passwordStr = password;
-        }
-
-        @Override
-        protected void onPreExecute() {
-            //if you want to do something before doInBackground
-        }
-
-        @Override
-        protected Boolean doInBackground(Void... params) {
-            //this is where you should write your authentication code
-            // or call external service
-            // following try-catch just simulates network access
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                return false;
-            }
-
-            //using a local dummy credentials store to authenticate
-            String[] pieces = DUMMY_CREDENTIALS.split(":");
-            if (pieces[0].equals(emailStr) && pieces[1].equals(passwordStr)) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-
-        @Override
-        protected void onPostExecute(final Boolean success) {
-            userLoginTask = null;
-            //stop the progress spinner
-            showProgress(false);
-
-            if (success) {
-                Intent intent=new Intent(MainActivity.this,HomeActivity.class);
-                startActivity(intent);
-            } else {
-                //// login failure
-                passwordTextView.setError("Wrong Parameters");
-                passwordTextView.requestFocus();
-            }
-        }
-
-        @Override
-        protected void onCancelled() {
-            userLoginTask = null;
-            showProgress(false);
-        }
-    }
-
 }
