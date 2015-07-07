48bfcafbf60a99091e0ff9059bfbfa78375475a0
giliy <gili.yohanan@gmail.com>
2015-07-08 00:32:32 +0300

    last update 7.7

--- app/src/main/java/com/barcodescanner/gili/scan9/MainActivity.java
+++ app/src/main/java/com/barcodescanner/gili/scan9/MainActivity.java
@@ -2,30 +2,21 @@ package com.barcodescanner.gili.scan9;
 
 
 import android.app.Activity;
+import android.content.CursorLoader;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.PixelFormat;
 import android.graphics.drawable.BitmapDrawable;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.util.Log;
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
-import android.app.LoaderManager.LoaderCallbacks;
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.graphics.Paint;
 import android.net.Uri;
-import android.os.Build;
+import android.os.Bundle;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Loader;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
-import android.text.util.Linkify;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
@@ -41,7 +32,7 @@ public class MainActivity extends Activity implements LoaderCallbacks<Cursor> {
     private View progressView;
     private AutoCompleteTextView usernameTextView;
     private EditText passwordTextView;
-    private Button signUpButton;
+    private TextView createAcount;
     private Button loginButton;
 
     @Override
@@ -66,11 +57,74 @@ public class MainActivity extends Activity implements LoaderCallbacks<Cursor> {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
                 if (id == EditorInfo.IME_NULL) {
+                    initLogin();
                     return true;
                 }
                 return false;
             }
         });
+
+        loginButton = (Button) findViewById(R.id.Login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initLogin();
+            }
+        });
+
+        createAcount = (TextView) findViewById(R.id.createAcountText);
+        createAcount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.this.startActivity(new Intent(MainActivity.this,RegisterScreen.class));
+            }
+        });
+    }
+
+    public void initLogin() {
+
+        usernameTextView.setError(null);
+        passwordTextView.setError(null);
+
+        String username = usernameTextView.getText().toString();
+        String password = passwordTextView.getText().toString();
+        boolean cancelLogin = false;
+        View focusView = null;
+
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            passwordTextView.setError("Invalid Password");
+            focusView = passwordTextView;
+            cancelLogin = true;
+        }
+
+        if (TextUtils.isEmpty(username)) {
+            usernameTextView.setError("Empty Username");
+            focusView = usernameTextView;
+            cancelLogin = true;
+        } else if (!isEmailValid(username)) {
+            usernameTextView.setError("Invalid Username");
+            focusView = usernameTextView;
+            cancelLogin = true;
+        }
+
+        if (cancelLogin) {
+            // error in login
+            focusView.requestFocus();
+        } else {
+            // show progress spinner, and start background task to login
+            Intent intent=new Intent(MainActivity.this,HomeActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private boolean isPasswordValid(String password) {
+        return password.length() > 4;
+        //add your own logic
+    }
+
+    private boolean isEmailValid(String email) {
+        //add your own logic
+        return email.contains("@");
     }
 
     private void loadAutoComplete() {
@@ -80,12 +134,50 @@ public class MainActivity extends Activity implements LoaderCallbacks<Cursor> {
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        return null;
+        return new CursorLoader(this,
+                // Retrieve data rows for the device user's 'profile' contact.
+                Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
+                        ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
+
+                // Select only email addresses.
+                ContactsContract.Contacts.Data.MIMETYPE +
+                        " = ?", new String[]{ContactsContract.CommonDataKinds.Email
+                .CONTENT_ITEM_TYPE},
+
+                // Show primary email addresses first. Note that there won't be
+                // a primary email address if the user hasn't specified one.
+                ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
+    }
+
+    private interface ProfileQuery {
+        String[] PROJECTION = {
+                ContactsContract.CommonDataKinds.Email.ADDRESS,
+                ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
+        };
+
+        int ADDRESS = 0;
+        int IS_PRIMARY = 1;
     }
 
     @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        List<String> emails = new ArrayList<>();
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            emails.add(cursor.getString(ProfileQuery.ADDRESS));
+            cursor.moveToNext();
+        }
 
+        addEmailsToAutoComplete(emails);
+    }
+
+    private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
+        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<>(MainActivity.this,
+                        android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
+
+        usernameTextView.setAdapter(adapter);
     }
 
     @Override
